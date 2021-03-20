e57b2dfa6e2847d23514f7e8e57d9c61d9ad6a6e
47909 <479094092@qq.com>
2021-03-20 18:06:04 +0800

    新增frame处理

--- test_frame.py
+++ test_frame.py
@@ -1,1 +1,13 @@
 # -*- coding:utf-8 -*-
+from setdown import Fixture
+
+
+class TestFrame(Fixture):
+    def test_frame(self):
+        self.driver.get('https://www.runoob.com/try/try.php?filename=jqueryui-api-droppable')
+        # frame 切换
+        self.driver.switch_to.frame('iframeResult')
+        print(self.driver.find_element_by_id('draggable').text)
+        # 切回默认frame
+        self.driver.switch_to.default_content()
+        print(self.driver.find_element_by_id('submitBTN').text)
