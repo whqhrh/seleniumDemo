f1e005b495a2826d7068c873d858d1fa57094c31
47909 <479094092@qq.com>
2021-03-20 18:36:44 +0800

    新增多浏览器处理

--- setdown.py
+++ setdown.py
@@ -1,10 +1,16 @@
 # -*- coding:utf-8 -*-
+import os
+
 from selenium import webdriver
 
 
 class Fixture:
     def setup(self):
-        self.driver = webdriver.Chrome()
+        browser = os.getenv()
+        if browser == 'chrome':
+            self.driver = webdriver.Chrome()
+        elif browser == 'firefox':
+            self.driver = webdriver.firefox()
         self.driver.maximize_window()
         self.driver.implicitly_wait(5)
 
