531d54ece2735daab3854b691aeb65956d2e4310
47909 <479094092@qq.com>
2021-03-20 17:38:21 +0800

    前后置整合

--- setdown.py
+++ setdown.py
@@ -1,1 +1,12 @@
 # -*- coding:utf-8 -*-
+from selenium import webdriver
+
+
+class Fixture:
+    def setup(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(5)
+
+    def teardown(self):
+        self.driver.quit()
