d6809b275b8fe76d97c044ee9741b66d019dffca
47909 <479094092@qq.com>
2021-03-21 10:37:33 +0800

    新增JS代码块处理

--- test_js.py
+++ test_js.py
@@ -1,1 +1,17 @@
 # -*- coding:utf-8 -*-
+from time import sleep
+
+from setdown import Fixture
+
+
+class TestJs(Fixture):
+    def test_js_scroll(self):
+        self.driver.get('https://www.baidu.com')
+        self.driver.find_element_by_id('kw').send_keys('selenium测试')
+        element = self.driver.execute_script("return document.getElementById('su')")
+        element.click()
+        sleep(3)
+        self.driver.execute_script("return document.scrollingElement.scrollTop=10000")
+        sleep(3)
+        self.driver.find_element_by_xpath('//*[@id="page"]/div/a[10]').click()
+        sleep(5)
