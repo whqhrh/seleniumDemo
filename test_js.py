682cd45cc254cad607bcb845be71f16151ac0cd5
47909 <479094092@qq.com>
2021-03-21 21:52:00 +0800

    新增JS代码块处理

--- test_js.py
+++ test_js.py
@@ -1,10 +1,13 @@
 # -*- coding:utf-8 -*-
 from time import sleep
 
+import pytest
+
 from setdown import Fixture
 
 
 class TestJs(Fixture):
+    @pytest.mark.skip
     def test_js_scroll(self):
         self.driver.get('https://www.baidu.com')
         self.driver.find_element_by_id('kw').send_keys('selenium测试')
@@ -22,3 +25,15 @@ class TestJs(Fixture):
             'return document.title', 'return JSON.stringify(performance.timing)'
         ]:
             print(self.driver.execute_script(code))
+
+    def test_datetime(self):
+        self.driver.get("https://www.12306.cn/index/")
+        date = "document.getElementById('train_date').removeAttribute('readonly')"
+        self.driver.execute_script(date)
+        sleep(3)
+        date1 = "document.getElementById('train_date').value='2021-12-30'"
+        self.driver.execute_script(date1)
+        sleep(3)
+        print(f"当前日期为：{self.driver.find_element_by_id('train_date').text}......")
+        sleep(5)
+
